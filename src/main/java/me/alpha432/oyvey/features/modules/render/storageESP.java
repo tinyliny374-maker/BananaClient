aff7ae4ea2692d8d567be5eb77a8e2f9781bec9c
tinyliny374-maker <tinyliny374@gmail.com>
2026-04-06 13:29:07 -0400

    Create storageESP.java

---
+++ src/main/java/me/alpha432/oyvey/features/modules/render/storageESP.java
@@ -0,0 +1,19 @@
+package com.ovey.client.modules.render;
+
+import com.ovey.client.module.Module;
+import com.ovey.client.module.Category;
+
+public class StorageESP extends Module {
+
+    public StorageESP() {
+        super("StorageESP", "Highlights storage blocks", Category.RENDER);
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
