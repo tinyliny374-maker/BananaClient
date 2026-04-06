0529ad654053fcffa326593d739b85551f341a64
tinyliny374-maker <tinyliny374@gmail.com>
2026-04-06 13:30:34 -0400

    Update storageESP.java

--- src/main/java/me/alpha432/oyvey/features/modules/render/storageESP.java
+++ src/main/java/me/alpha432/oyvey/features/modules/render/storageESP.java
@@ -3,17 +3,36 @@ package com.ovey.client.modules.render;
 import com.ovey.client.module.Module;
 import com.ovey.client.module.Category;
 
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.block.entity.BarrelBlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.BlockPos;
+
 public class StorageESP extends Module {
+
+    private final MinecraftClient mc = MinecraftClient.getInstance();
 
     public StorageESP() {
         super("StorageESP", "Highlights storage blocks", Category.RENDER);
     }
 
     @Override
-    public void onEnable() {
-    }
+    public void onTick() {
+        if (mc.world == null) return;
 
-    @Override
-    public void onDisable() {
+        for (BlockEntity blockEntity : mc.world.blockEntities) {
+
+            if (blockEntity instanceof ChestBlockEntity ||
+                blockEntity instanceof ShulkerBoxBlockEntity ||
+                blockEntity instanceof BarrelBlockEntity) {
+
+                BlockPos pos = blockEntity.getPos();
+
+                // This prints location (you can replace with ESP rendering)
+                System.out.println("Storage found at: " + pos.toShortString());
+            }
+        }
     }
 }
